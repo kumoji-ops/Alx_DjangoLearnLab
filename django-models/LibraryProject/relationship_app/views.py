ab77a5483d0ead9c3569acdabfd64cc7a7d85270
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-07-20 17:30:06 +0100

    hhhh

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
