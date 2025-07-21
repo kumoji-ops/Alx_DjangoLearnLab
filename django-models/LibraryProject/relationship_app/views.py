b5a7e7e408ed1f2dc5695a8de09cb40fdc0cf137
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-07-21 23:22:57 +0100

    lll

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Book, Library, UserProfile 
 from .forms import BookForm
