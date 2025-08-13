c7cc898f1d5ae8f8b3bf0448122c0648b76340ce
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-08-13 16:34:17 +0100

    aaa

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import register_view, CustomLoginView, CustomLogoutView, profile_view
+from .views import home_view, register_view, CustomLoginView, CustomLogoutView, profile_view,  PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', home_view, name='home'), 
     path('register/', register_view, name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('profile/', profile_view, name='profile')
+    path('profile/', profile_view, name='profile'),
+    
+    # Blog posts CRUD
+    path('posts/', PostListView.as_view(), name='posts'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,8 +3,17 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import CustomUserCreationForm, ProfileUpdateForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+
+
+def home_view(request):
+    return render(request, 'blog/home.html') 
 
 def register_view(request):
     if request.method == 'POST':
@@ -34,4 +43,46 @@ def profile_view(request):
         else:
             form = ProfileUpdateForm(instance=request.user)
         return render(request, 'blog/profile.html', {'form': form})    
+    
+# List all posts
+class PostListView(ListView):
+    model = Post   
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+#View single post
+class PostDetailView(DetailView):
+    model = Post   
+    template_name = 'blog/post_detail.html'
+
+#Creat post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+# Update post (author only)
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post    
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+# delete post (author only)
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):    
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('posts')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
     
