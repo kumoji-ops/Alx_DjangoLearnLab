e253498de6e31540d6a3255645b6c45ab31d728c
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-08-24 16:27:00 +0100

    dff

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,44 +1,43 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+
 class UserSerializer(serializers.ModelSerializer):
-      followers_count = serializers.SerializerMethodField(read_only=True)
-      following_count = serializers.SerializerMethodField(read_only=True)
+    followers_count = serializers.SerializerMethodField(read_only=True)
+    following_count = serializers.SerializerMethodField(read_only=True)
 
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name',
+            'bio', 'profile_picture', 'followers_count', 'following_count',
+        ]
+        read_only_fields = ['id', 'followers_count', 'following_count']
 
-class Meta:
-      model = User
-      fields = [
-          'id', 'username', 'email', 'first_name', 'last_name',
-          'bio', 'profile_picture', 'followers_count', 'following_count',
-   ]
-      read_only_fields = ['id', 'followers_count', 'following_count']
+    def get_followers_count(self, obj):
+        return obj.followers.count()
 
-
-def get_followers_count(self, obj):
-      return obj.followers.count()
-
-
-def get_following_count(self, obj):
-      return obj.following.count()
+    def get_following_count(self, obj):
+        return obj.following.count()
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-      password = serializers.CharField(write_only=True, min_length=8)
+    password = serializers.CharField(write_only=True, min_length=8)
 
-class Meta:
-      model = User
-      fields = ['username', 'email', 'password']
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
 
-
-def create(self, validated_data):
-      user = User(
-      username=validated_data['username'],
-      email=validated_data.get('email', ''),
-    )
-      user.set_password(validated_data['password'])
-      user.save()
-      return user
+    def create(self, validated_data):
+        # Use create_user to handle password hashing
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        # Create auth token immediately
+        Token.objects.create(user=user)
+        return user
