95e1b756e022ec0ef7b41ee6d6e66a7b0d655c02
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-08-24 16:58:22 +0100

    add posts urls path

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('', api_root),  # <-- root URL
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
+    path('api/posts/', include('posts.urls')),  
 ]
 
 if settings.DEBUG:
