f0a5d2638c18b3b24e7a02a0b760c723d93a12d3
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-08-24 16:56:44 +0100

    Removed duplicate CustomUser from posts app; use accounts.User as single custom user model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AbstractUser
 
 User = get_user_model()
 
@@ -18,16 +17,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
-
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 
     # Local
     'accounts',
+    'posts',
+    'notifications',
 ]
 
 MIDDLEWARE = [
